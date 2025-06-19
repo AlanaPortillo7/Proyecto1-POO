7f92d18b3bdb17229a21a0aed84556e5951e8c1c
AlanaPortillo7 <alana2pvart@gmail.com>
2025-06-19 14:16:53 -0600

    1.0.1

--- test_modelo.py
+++ test_modelo.py
@@ -1,24 +1,32 @@
 import unittest
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
 from bson.objectid import ObjectId
 from classes import Piloto, Coche
-from app import get_collection, URI
+
+load_dotenv()
 
 class TestRelacionPilotoCoche(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.pilotos_col = get_collection(URI, db="oop_db", col="pilotos")
-        cls.coches_col = get_collection(URI, db="oop_db", col="coches")
-        cls.test_ids = []
+        cls.uri = os.getenv("URI")
+        cls.client = MongoClient(cls.uri, tls=True, tlsAllowInvalidCertificates=True)
+        db = cls.client["oop_db"]
+        cls.pilotos_col = db["pilotos"]
+        cls.coches_col = db["coches"]
+        cls.pilotos_ids = []
+        cls.coches_ids = []
 
     def test_creacion_piloto_y_coche(self):
         piloto = Piloto("Carlos Sainz", "España", 29)
         piloto_id = piloto.save(self.pilotos_col)
-        self.test_ids.append(ObjectId(piloto_id))
+        self.pilotos_ids.append(ObjectId(piloto_id))
 
         coche = Coche("Ferrari", "SF-24", 2024, piloto_id)
         coche_id = coche.save(self.coches_col)
-        self.test_ids.append(ObjectId(coche_id))
+        self.coches_ids.append(ObjectId(coche_id))
 
         self.assertIsNotNone(piloto_id)
         self.assertIsNotNone(coche_id)
@@ -29,11 +37,11 @@ class TestRelacionPilotoCoche(unittest.TestCase):
     def test_actualizacion_relacion(self):
         piloto = Piloto("Oscar Piastri", "Australia", 23)
         piloto_id = piloto.save(self.pilotos_col)
-        self.test_ids.append(ObjectId(piloto_id))
+        self.pilotos_ids.append(ObjectId(piloto_id))
 
         coche = Coche("McLaren", "MCL38", 2024, piloto_id)
         coche_id = coche.save(self.coches_col)
-        self.test_ids.append(ObjectId(coche_id))
+        self.coches_ids.append(ObjectId(coche_id))
 
         piloto.update(self.pilotos_col, piloto_id, coche_id)
 
@@ -41,14 +49,17 @@ class TestRelacionPilotoCoche(unittest.TestCase):
         self.assertEqual(piloto_actualizado["coche_id"], coche_id)
 
     def tearDown(self):
-        for _id in self.test_ids:
+        for _id in self.pilotos_ids:
             self.pilotos_col.delete_one({"_id": _id})
+        for _id in self.coches_ids:
             self.coches_col.delete_one({"_id": _id})
-        self.test_ids.clear()
+        self.pilotos_ids.clear()
+        self.coches_ids.clear()
 
     @classmethod
     def tearDownClass(cls):
-        cls.pilotos_col.database.client.close()
+        cls.client.close()
 
 if __name__ == '__main__':
     unittest.main()
+
